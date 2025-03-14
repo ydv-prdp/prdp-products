d7e1d0fc777c913586a471bee32f4f436163ab87
ydv-prdp <pradeepyadav101010@gmail.com>
2025-03-14 10:14:01 +0530

    added dark and light theme

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "./mode-toggle"
+
+const Header = () => {
+  return (
+    <header className="flex justify-end p-4">
+        <ModeToggle/>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/navbar.tsx
@@ -0,0 +1,32 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+import { ModeToggle } from "./mode-toggle"
+
+
+  
+  const Navbar = () => {
+    return (
+        <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>  
+    )
+  }
+  
+  export default Navbar
